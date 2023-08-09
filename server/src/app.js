649fe50480f2eeb6a7fef94d866aef90d3e4998c
Nayo Deeyo <call0979758170@gmail.com>
2023-08-09 12:42:58 +0700

    '001'

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,12 @@
-console.log('Hello nodejs server')
+let express = require('express')
+const app = express()
+
+app.get('/status', function (req, res) {
+    res.send('Hello nodejs server')
+})
+
+let port = 8081
+app.listen(port, function () {
+    console.log('server running on ' + port)
+})
+
