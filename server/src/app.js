ee5c9d0085e89b6540b7b1d57a0c09f5be803999
Nayo Deeyo <call0979758170@gmail.com>
2023-08-09 12:58:33 +0700

    '001'

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,22 @@ app.get('/status', function (req, res) {
     res.send('Hello nodejs server')
 })
 
+app.get('/hello/:person', function (req,res) {
+    console.log('hello - ' + req.params.person)
+    res.send('sey hello eith' + req.params.person)
+})
+//get user by id
+app.get('/user/:userId', function (req, res) {
+    res.send('ดูข้อมูลผู้ใช้งาน')
+})
+
+//get all user
+app.get('/user', function (req, res) {
+    res.send('เรียกข้อมูลผู้ใช้งาน')
+})
+
 let port = 8081
+
 app.listen(port, function () {
     console.log('server running on ' + port)
 })
